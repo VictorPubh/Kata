5167bd90d4edfea31107ad07987a30a543d541bd
João Victor <joaosantana@intnet.com.br>
2021-12-24 20:36:15 -0300

    Kata: IsPangram on Php

--- PHP/src/StringMethods.php
+++ PHP/src/StringMethods.php
@@ -1,34 +1,62 @@
 <?php
 
-/**
- * Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
- * 
- * Example:
- * 
- * StringMethods::containsEnding("abc", "bc"); // true
- * StringMethods::containsEnding("abc", "d");  // false 
- */
-
 namespace Kata;
 
 class StringMethods
 {
     public $string;
 
-    function __construct($str, $ending)
+    function __construct($str)
     {
-        $this->containsEnding($str, $ending);
+        $this->string = $str;
     }
 
     /**
-     * ContainsEnding is case-sensitive
+     * Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+     * 
+     * Example:
+     * 
+     * StringMethods::containsEnding("abc", "bc"); // true
+     * StringMethods::containsEnding("abc", "d");  // false 
+     * 
+     * ContainsEnding is case-sensitive /!\
      * */
-    public function containsEnding($str, $ending)
-    {
-        $str_length = strlen($str);
-        $end_length = strlen($ending);
-        $pos_ending = $str_length - $end_length;
 
-        return substr($str, $pos_ending, $str_length) === $ending;
+    public function containsEnding($word, $ending)
+    {
+        $word_length = strlen($word);
+        $end_length = strlen($ending);
+        $pos_ending = $word_length - $end_length;
+
+        return substr($word, $pos_ending) === $ending;
+    }
+
+    /**
+     * A pangram is a sentence that contains every single letter of the alphabet at least once. For example,
+     * the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z
+     * at least once (case is irrelevant).
+     * 
+     * Given a string, detect whether or not it is a pangram. Return True if it is, False if not.Ignore numbers and punctuation.
+     */
+
+    public function isPangram($phrase)
+    {
+        $phrase = strtolower($phrase);
+        $phrase_array = str_split($phrase, 1);
+        $alphas = range('a', 'z');
+
+        foreach ($alphas as &$letter) {
+            if (in_array($letter, $phrase_array)) {
+                $letter = true;
+            }
+        }
+
+        foreach ($alphas as $letter) {
+            if ($letter !== true) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- PHP/tests/StringMethodsTest.php
+++ PHP/tests/StringMethodsTest.php
@@ -2,13 +2,20 @@
 
 use Kata\StringMethods;
 
-it('Codewars Test Recfactory', function () {
-    $this->assertEquals(true, StringMethods::containsEnding("Samurai", "ai"));
-    $this->assertEquals(false, StringMethods::containsEnding("sumo", "omo"));
+it('Codewars Test Recfactory StringMehtods::containsEnding', function () {
+    $this->assertEquals(true, StringMethods::containsEnding("abc", "bc"));
     $this->assertEquals(true, StringMethods::containsEnding("ninja", "ja"));
     $this->assertEquals(true, StringMethods::containsEnding("sensei", "i"));
+
+    $this->assertEquals(false, StringMethods::containsEnding("sumo", "omo"));
     $this->assertEquals(false, StringMethods::containsEnding("samurai", "ra"));
     $this->assertEquals(false, StringMethods::containsEnding("abc", "abcd"));
-    $this->assertEquals(true, StringMethods::containsEnding("abc", "abc"));
-    $this->assertEquals(true, StringMethods::containsEnding("abcabc", "bc"));
+});
+
+it('Codewars Test Recfactory StringMehtods::isPangram', function () {
+    $this->assertEquals(true, StringMethods::isPangram("The quick brown fox jumps over the lazy dog."));
+    $this->assertEquals(true, StringMethods::isPangram("Vi que ex-janota gordo fez show com playback."));
+
+    $this->assertEquals(false, StringMethods::isPangram("Jovem ex-quenga picha frase da Blitz."));
+    $this->assertEquals(false, StringMethods::isPangram("This is not a pangram."));
 });
