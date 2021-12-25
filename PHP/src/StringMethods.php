c0d2add896c12573c94af20c7eff9c18c807fbfb
João Victor <joaosantana@intnet.com.br>
2021-12-24 21:33:06 -0300

    Kata: isIsogram on Php

--- PHP/src/StringMethods.php
+++ PHP/src/StringMethods.php
@@ -61,6 +61,16 @@ class StringMethods
         return true;
     }
 
+    /**
+     * An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+     * Implement a function that determines whether a string that contains only letters is an isogram.
+     * Assume the empty string is an isogram. Ignore letter case.
+     * 
+     * "Dermatoglyphics" --> true
+     * "aba" --> false
+     * "moOse" --> false (ignore letter casing)
+     */
+
     public function isIsogram($phrase)
     {
         $phrase_array = Auxiliary::lowerAndSplit($phrase, true);
